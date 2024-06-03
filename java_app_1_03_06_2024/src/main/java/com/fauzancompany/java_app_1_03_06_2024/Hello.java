e3cc6fc7c7381f1b87e3f08bb605d292b0fbb46f
D-23 <D-23@D-23>
2024-06-03 20:15:37 +0700

    New Class Hello World 03 May 2024

---
+++ java_app_1_03_06_2024/src/main/java/com/fauzancompany/java_app_1_03_06_2024/Hello.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fauzancompany.java_app_1_03_06_2024;
+
+/**
+ *
+ * @author D-23
+ */
+public class Hello {
+    public static void main(String[] args) {
+        int umur = 99;
+       
+        System.out.println("-------------------------------------------");
+        System.out.println("----------------FIRST PROGRAM--------------");
+        System.out.print("Hello Aku Fauzan, ");
+        System.out.print("Salam kenal teman-teman");
+        System.out.println("Aku berumur berapa hayoooo?" + umur);
+        System.out.println("Aku sedang kuliah di Institut Asia Malang");
+        System.out.println("Hobiku adalah Makan dan Tidur");
+        System.out.println("");
+        System.out.println("-------------------------------------------");
+    }
+}
