e0381cdf3b7f59a69ed33defc6f8771e2a458673
D-23 <D-23@D-23>
2024-06-03 20:21:32 +0700

    Update Output with Variable Class Hello World 03 May 2024

--- java_app_1_03_06_2024/src/main/java/com/fauzancompany/java_app_1_03_06_2024/Hello.java
+++ java_app_1_03_06_2024/src/main/java/com/fauzancompany/java_app_1_03_06_2024/Hello.java
@@ -11,15 +11,21 @@ package com.fauzancompany.java_app_1_03_06_2024;
  */
 public class Hello {
     public static void main(String[] args) {
+        
+        // Init Variabel
         int umur = 99;
-       
+        String nama = "Ahmad Fauzan Arif";
+        String akademik = "Institut Asia Malang";
+        String hobi = "Makan dan Tidur";
+        
+        // Output
         System.out.println("-------------------------------------------");
         System.out.println("----------------FIRST PROGRAM--------------");
-        System.out.print("Hello Aku Fauzan, ");
-        System.out.print("Salam kenal teman-teman");
-        System.out.println("Aku berumur berapa hayoooo?" + umur);
-        System.out.println("Aku sedang kuliah di Institut Asia Malang");
-        System.out.println("Hobiku adalah Makan dan Tidur");
+        System.out.print("Hello Aku "+ nama);
+        System.out.println("Salam kenal teman-teman");
+        System.out.println("Aku berumur berapa hayoooo?" + "Apa?" + umur);
+        System.out.println("Aku sedang kuliah di " + akademik);
+        System.out.println("Hobiku adalah " + hobi);
         System.out.println("");
         System.out.println("-------------------------------------------");
     }
